69e65b298b1a50603db438c3aba4e84a65a994f2
vanya <vanya20074@gmail.com>
2015-03-14 14:24:31 +0200

    add notify-send use

--- src/main/java/org/vanya/Notification.java
+++ src/main/java/org/vanya/Notification.java
@@ -1,5 +1,7 @@
 package org.vanya;
 
+import java.io.IOException;
+
 /**
  * Created by vanya on 13.03.15.
  */
@@ -28,4 +30,24 @@ public class Notification {
     public void setText(String text) {
         this.text = text;
     }
+
+    public boolean send(){
+
+        if(exec("notify-send -i empathy -u critical " + "\"" + title + "\" \"" + text + "\"")) return true;
+        else return false;
+    }
+    private static boolean exec(String command) {
+
+        String[] cmd = {"/bin/sh", "-c", command};
+
+        try {
+            Process p = Runtime.getRuntime().exec(cmd);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
 }
